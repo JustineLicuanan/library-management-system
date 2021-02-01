01ad16a1f59e92db5693de925becb1da160e2d87
Justine Licuanan <caissoone@gmail.com>
2021-02-01 21:55:48 +0800

    Redeploy to invalidate cache

--- src/meta.ts
+++ src/meta.ts
@@ -1,7 +1,7 @@
 export const myMeta = {
 	website: 'https://libraryms.now.sh',
 	graphqlEndpoint:
-		process.env.GRAPHQL_ENDPOINT || 'http://127.0.0.1:4000/graphql',
+		process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
 	title: 'Library MS',
 	longTitle: 'Library Management System',
 	description: 'Made using PERN Stack with GraphQL',
