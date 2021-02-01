4b1cacf2a4608235a033788682d25b2d03251acf
Justine Licuanan <70696164+JustineLicuanan@users.noreply.github.com>
2021-02-01 22:15:59 +0800

    Prefix GRAPHQL_ENDPOINT env var with REACT_APP_

--- src/meta.ts
+++ src/meta.ts
@@ -1,7 +1,7 @@
 export const myMeta = {
 	website: 'https://libraryms.now.sh',
 	graphqlEndpoint:
-		process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
+		process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
 	title: 'Library MS',
 	longTitle: 'Library Management System',
 	description: 'Made using PERN Stack with GraphQL',
