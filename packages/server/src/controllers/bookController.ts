1eae314491a8748c2ef2bffdeba1fbca286254ea
Justine Licuanan <caissoone@gmail.com>
2021-03-03 16:49:05 +0800

    Create "add book" controller

--- packages/server/src/controllers/bookController.ts
+++ packages/server/src/controllers/bookController.ts
@@ -3,12 +3,27 @@ import { ParamsDictionary } from 'express-serve-static-core';
 
 import { Book } from '../entity/Book';
 
-export const POST_addBook: RequestHandler<
-	ParamsDictionary,
-	any,
-	Book
-> = async () => {
-	//
+export const POST_addBook: RequestHandler<ParamsDictionary, any, Book> = async (
+	req,
+	res
+) => {
+	const bookExists = await Book.findOne({ ISBN: req.body.ISBN });
+
+	if (bookExists) {
+		res.status(400).json({
+			errors: [
+				{
+					path: 'ISBN',
+					message: 'ISBN is already used',
+				},
+			],
+		});
+
+		return;
+	}
+
+	const book = await Book.create(req.body).save();
+	res.json({ book });
 };
 
 export const GET_books = async () => {
